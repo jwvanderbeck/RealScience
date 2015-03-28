bf0585109fbb45395b0902a436805336d9f6abf9
John Vanderbeck <jwvanderbeck@gmail.com>
2015-03-28 09:44:32 -0700

    New Condition: GeeForce

---
+++ source/Conditions/RealScienceCondition_GeeForce.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace RealScience.Conditions
+{
+    class RealScienceCondition_GeeForce : RealScienceCondition
+    {
+        // common properties
+        public string conditionType = "GeeForce";
+        public bool restriction = false;
+        public string exclusion = "";
+        public float dataRateModifier = 1f;
+        // specific properties
+        public float gMin = 0f;
+        public float gMax = float.MaxValue;
+
+        public override float DataRateModifier
+        {
+            get { return dataRateModifier; }
+        }
+        public override bool IsRestriction
+        {
+            get { return restriction; }
+        }
+        public override string Exclusion
+        {
+            get { return exclusion; }
+        }
+
+        public override bool Evaluate(Part part, float deltaTime)
+        {
+            return FlightGlobals.ship_geeForce >= gMin && FlightGlobals.ship_geeForce <= gMax;
+        }
+        public override void Load(ConfigNode node)
+        {
+            // Load common properties
+            if (node.HasValue("conditionType"))
+                conditionType = node.GetValue("conditionType");
+            if (node.HasValue("exclusion"))
+                exclusion = node.GetValue("exclusion");
+            if (node.HasValue("restriction"))
+            {
+                try
+                {
+                    restriction = bool.Parse(node.GetValue("restriction"));
+                }
+                catch (FormatException)
+                {
+                    restriction = false;
+                }
+            }
+            if (node.HasValue("dataRateModifier"))
+            {
+                try
+                {
+                    dataRateModifier = float.Parse(node.GetValue("dataRateModifier"));
+                }
+                catch (FormatException)
+                {
+                    dataRateModifier = 1f;
+                }
+            }
+            // Load specific properties
+            if (node.HasValue("gMin"))
+                gMin = float.Parse(node.GetValue("gMin"));
+            if (node.HasValue("gMax"))
+                gMax = float.Parse(node.GetValue("gMax"));
+        }
+        public override void Save(ConfigNode node)
+        {
+            // Save common properties
+            node.AddValue("conditionType", conditionType);
+            node.AddValue("restriction", restriction);
+            node.AddValue("exclusion", exclusion);
+            node.AddValue("dataRateModifier", dataRateModifier);
+            node.AddValue("gMin", gMin);
+            node.AddValue("gMax", gMax);
+        }
+    }
+}
