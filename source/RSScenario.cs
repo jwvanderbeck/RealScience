4fb6f0a638d14a722d73d33caa15010e65a13a10
John Vanderbeck <jwvanderbeck@gmail.com>
2015-05-27 18:28:52 -0700

    * Add barebones Scenario module.  Will need to be fleshed out as other pieces come into place.

---
+++ source/RSScenario.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using KSPPluginFramework;
+
+namespace RealScience
+{
+	[KSPScenario(ScenarioCreationOptions.AddToAllGames, 
+		new GameScenes[] 
+		{ 
+			GameScenes.FLIGHT,
+			GameScenes.EDITOR,
+			GameScenes.SPACECENTER,
+			GameScenes.TRACKSTATION
+		}
+	)]
+	public class RSScenario : ScenarioModule
+	{
+		public static RSScenario Instance { get; private set; }
+		public bool isReady = false;
+
+		internal void Log(string message)
+		{
+			bool debug = true;
+			if (debug)
+				Debug.Log("[RealScience] " + message);
+		}
+
+
+		// KSP Methods
+		// ScenarioModule Methods
+		public override void OnAwake ()
+		{
+			base.OnAwake ();
+
+			Instance = this;
+		}
+		public void Start()
+		{
+			Log("Scenario Start");
+			isReady = true; 
+		}
+
+		public override void OnLoad(ConfigNode node)
+		{
+			base.OnLoad(node);
+		}
+
+		public override void OnSave(ConfigNode node)
+		{
+			base.OnSave(node);
+		}
+	}
+}
+
